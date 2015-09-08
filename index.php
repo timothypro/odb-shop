6325a63c1f341f36d3369b8e0fcc2eab1c008934
Timothy Zhou <timothyzhou@vip.qq.com>
2015-09-08 15:08:19 +0700

    re-initialized the project

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/main.css" media="screen" title="no title" charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <title>Document</title>
+</head>
+
+<body>
+  <div class="wrapper">
+    <nav class="header">
+      <a href="#">
+        <img src="img/logo.png" alt="" class="logo">
+      </a>
+    </nav>
+
+    <aside class="item item-img">
+      <img src="img/white-book.png" alt="" />
+    </aside>
+    <article class="item-text">
+      <h1>白色日记</h1>
+      <p>白色日记集结了许多大自然的图片，这些都是上帝奇妙美好的创造。 每一个画面都是美好的瞬间；每一道风景都是由资深摄影师苏其忠所拍摄。 无论是大小行程，还是生活点滴，你都能装进这本日记里。 盼望这本富有生命力的日记能走入你的生活，里头的文字带给你鼓励与反思， 且伴随你同走这一年，透过大自然认识上帝。 白色日记使用小贴士：
+      </p>
+      <p>•每月月历设有全年读经表，你可以按照读经表的进度每日阅读上帝的话语。 日日读经，灵命更新。
+      </p>
+      <p>•每月阅读一篇文章，每周设有与文章相关的反思题或祷告，祈盼你能从这些文章中得着激励， 盼望和安慰。</p>
+      <p>•日记中间附有多张小卡片，希望你能使用这些小卡向身边的人表达感谢和关爱。</p>
+      <div class="price">￥100</div>
+      <a href="#" class="buynow">buy now</a>
+    </article>
+
+  </div>
+</body>
+
+</html>
